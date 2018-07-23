e42b41fc9691475b72341baffdca06d3676153e5
Ulises Santana <ulisesantana@gmail.com>
2018-07-23 21:44:24 +0100

    Add /request route and trusted proxy

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import requestIp from 'request-ip';
+import JSON from 'circular-json';
 
 
 export default function Server() {
@@ -18,10 +19,14 @@ export default function Server() {
   }));
   api.use(bodyParser.json());
   api.set('view engine', 'pug');
+  api.set('trust proxy', 'loopback');
 
   // API Routes
   api.get('/', (req, res, next) => {
     res.render('ip', { title: 'Your IP Address', ip: req.clientIp});
+  });
+  api.get('/request', (req, res, next) => {
+    res.status(200).send(JSON.stringify(req, null, 2));
   });
 
   api.listen(PORT, () => log(`REST API listening on port ${PORT}.`));
